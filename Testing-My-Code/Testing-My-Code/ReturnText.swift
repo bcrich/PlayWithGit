54d94b6d4b6e594272cc2eac052e596aa4fcb166
Naomi Schettini <nschetti@ford.com>
2020-08-22 17:23:22 -0400

    Created our LocoFramework

---
+++ Testing-My-Code/Testing-My-Code/ReturnText.swift
@@ -0,0 +1,21 @@
+//
+//  ReturnText.swift
+//  Testing-My-Code
+//
+//  Created by Macbook on 8/22/20.
+//  Copyright © 2020 Macbook. All rights reserved.
+//
+
+import Foundation
+
+public class ReturnText {
+
+    private init() {}
+
+        public static func randomText() {
+
+
+        }
+
+
+}
---
+++ Testing-My-Code/Testing-My-Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing-My-Code
+//
+//  Created by Macbook on 8/22/20.
+//  Copyright © 2020 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
